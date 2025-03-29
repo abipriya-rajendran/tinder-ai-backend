7fafe036f096b0bdbd8d4e55fff2b9a88895aa2a
abipriya <abipriya.rajendran@gmail.com>
2025-03-29 17:02:32 +0530

    Add a Profile to DB and fetch it

--- src/main/java/com/example/tinderaibackend/TinderAiBackendApplication.java
+++ src/main/java/com/example/tinderaibackend/TinderAiBackendApplication.java
@@ -1,13 +1,37 @@
 package com.example.tinderaibackend;
 
+import com.example.tinderaibackend.profiles.Gender;
+import com.example.tinderaibackend.profiles.Profile;
+import com.example.tinderaibackend.profiles.ProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TinderAiBackendApplication {
+public class TinderAiBackendApplication implements CommandLineRunner {
+
+	@Autowired
+	ProfileRepository profileRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TinderAiBackendApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Profile profile = new Profile(
+				"1",
+				"abipriya",
+				"rajendran",
+				2,
+				"Indian",
+				Gender.FEMALE,
+				"SDE",
+				"foo.jpg",
+				"INTP"
+		);
+		profileRepository.save(profile);
+		profileRepository.findAll().forEach(System.out::println);
+	}
 }
