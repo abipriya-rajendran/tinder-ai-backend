6064dc85dbcc490b109b186616d91889c2c7fb03
abipriya <abipriya.rajendran@gmail.com>
2025-03-29 15:29:30 +0530

    Renamed the package

--- src/main/java/com/example/tinderaibackend/TinderAiBackendApplication.java
+++ src/main/java/com/example/tinderaibackend/TinderAiBackendApplication.java
@@ -1,4 +1,4 @@
-package com.example.tinder_ai_backend;
+package com.example.tinderaibackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/example/tinderaibackend/TinderAiBackendApplicationTests.java
+++ src/test/java/com/example/tinderaibackend/TinderAiBackendApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.tinder_ai_backend;
+package com.example.tinderaibackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
