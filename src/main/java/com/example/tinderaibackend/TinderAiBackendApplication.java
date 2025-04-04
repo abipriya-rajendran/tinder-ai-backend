b8adb896d0b2ab50d3ce4dd15c51c5b29b847462
abipriya <abipriya.rajendran@gmail.com>
2025-04-04 06:24:03 +0530

    Added profile creation service

--- src/main/java/com/example/tinderaibackend/TinderAiBackendApplication.java
+++ src/main/java/com/example/tinderaibackend/TinderAiBackendApplication.java
@@ -1,33 +1,16 @@
 package com.example.tinderaibackend;
 
-import com.example.tinderaibackend.conversation.ChatMessage;
-import com.example.tinderaibackend.conversation.Conversation;
-import com.example.tinderaibackend.conversation.ConversationRepository;
-import com.example.tinderaibackend.profiles.Gender;
-import com.example.tinderaibackend.profiles.Profile;
-import com.example.tinderaibackend.profiles.ProfileRepository;
-import org.springframework.ai.chat.model.ChatResponse;
-import org.springframework.ai.chat.prompt.Prompt;
-import org.springframework.ai.ollama.OllamaChatModel;
+import com.example.tinderaibackend.profiles.ProfileCreationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
 @SpringBootApplication
 public class TinderAiBackendApplication implements CommandLineRunner {
 
 	@Autowired
-	private ProfileRepository profileRepository;
-
-	@Autowired
-	private ConversationRepository conversationRepository;
-
-	@Autowired
-	private OllamaChatModel chatModel;
+	private ProfileCreationService profileCreationService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TinderAiBackendApplication.class, args);
@@ -35,38 +18,6 @@ public class TinderAiBackendApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) {
-		Prompt prompt = new Prompt("What is langchain?");
-		ChatResponse chatResponse = chatModel.call(prompt);
-		System.out.println(chatResponse.getResult().getOutput());
-
-		profileRepository.deleteAll();
-		conversationRepository.deleteAll();
-
-		Profile profile = new Profile(
-				"1",
-				"abipriya",
-				"rajendran",
-				2,
-				"Indian",
-				Gender.FEMALE,
-				"SDE",
-				"foo.jpg",
-				"INTP"
-		);
-
-		profileRepository.save(profile);
-		profileRepository.findAll().forEach(System.out::println);
-
-		Conversation conversation = new Conversation(
-				"1",
-				profile.id(),
-				List.of(
-						new ChatMessage("Hello!!", profile.id(), LocalDateTime.now())
-				)
-		);
-
-		conversationRepository.save(conversation);
-		conversationRepository.findAll().forEach(System.out::println);
-
+		profileCreationService.saveProfilesToDB();
 	}
 }
